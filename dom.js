d4f0cc240bc7bfb5111d42a3dafbb320edce7ec8
jhumbrac <john.humbracht@generalassemb.ly>
2019-11-19 12:52:53 -0600

    updated css and form elements

--- dom.js
+++ dom.js
@@ -1,22 +1,3 @@
-var closeBtn = $('<div>').attr('class', 'closeBtn').text('X');
-
-// new trip input
-var tripInput = $('<form>').attr('id', 'tripInput');
-var tripLocation = $('<input>').attr('type', 'text').attr('id', 'tripLocation');
-var locationLabel = $('<label>').attr('for', 'tripLocation').text('Where are you going?');
-var tripStartDate = $('<input>').attr('type', 'date').attr('id', 'tripStartDate');
-var tripStartLabel = $('<label>').attr('for', 'tripStartDate').text('Trip Start');
-var tripEndDate = $('<input>').attr('type', 'date').attr('id', 'tripEndDate');
-var tripEndLabel = $('<label>').attr('for', 'tripEndDate').text('Trip End');
-var tripBtn = $('<button>').text('Plan Trip!');
-
-$('body').append(tripInput);
-tripInput.append(tripLocation, locationLabel, tripStartDate, tripStartLabel, tripEndDate, tripEndLabel, tripBtn, closeBtn);
-
-// google places api
-var googleApiKey = 'AIzaSyA-b6Ob-hdWMHrJO5fp-G5GCm76jeHqGRc';
-
-
 // landing page dom
 var welcomePage = $('<div>').attr('id', 'welcomePage');
 var welcomeText = $('<h1>').text('Plan your trip');
@@ -30,10 +11,38 @@ $('body').prepend(welcomePage);
 welcomePage.append(welcomeText, welcomeSubText, newTripBtn, upcomingTripsDisplay);
 upcomingTripsDisplay.append(upcomingTripsHeader);
 
+
+// close button
+var closeBtn = $('<p>').attr('class', 'closeBtn').append($('<span>').text('X'));
+
+
+// new trip input
+var tripInput = $('<form>').attr('id', 'tripInput');
+var tripInputDiv1 = $('<div>');
+var tripInputDiv2 = $('<div>');
+var tripInputDiv3 = $('<div>');
+var tripLocation = $('<input>').attr('type', 'text').attr('id', 'tripLocation');
+var locationLabel = $('<label>').attr('for', 'tripLocation').text('Where are you going?');
+var tripStartDate = $('<input>').attr('type', 'date').attr('id', 'tripStartDate');
+var tripStartLabel = $('<label>').attr('for', 'tripStartDate').text('Trip Start');
+var tripEndDate = $('<input>').attr('type', 'date').attr('id', 'tripEndDate');
+var tripEndLabel = $('<label>').attr('for', 'tripEndDate').text('Trip End');
+var tripBtn = $('<button>').attr('id', 'tripBtn').text('Plan Trip!');
+
+$('body').append(tripInput);
+tripInput.append((tripInputDiv1.append(tripLocation, locationLabel)));
+tripInput.append((tripInputDiv2.append(tripStartDate, tripStartLabel)));
+tripInput.append((tripInputDiv3.append(tripEndDate, tripEndLabel)));
+tripInput.append(tripBtn, closeBtn);
+
 newTripBtn.on('click', event=>{
     event.preventDefault();
     $('body').toggleClass('newTripModal');
 });
 closeBtn.on('click', event=>{
     $('body').toggleClass('newTripModal');
+})
+tripBtn.on('click', event=>{
+    event.preventDefault();
+    console.log('trip btn click');
 })
