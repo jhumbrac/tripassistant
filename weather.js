017935885b3b9ba04cd7c36acbe392069a307ed8
Zax <ztalley@unifiedprincipal.com>
2019-11-19 23:00:06 -0600

    pulling weather data

--- weather.js
+++ weather.js
@@ -27,7 +27,21 @@ var tripLocation = 'Miami';//changed to a variable to test and make an easier ca
 	        "x-rapidapi-key": APIKey
         }
       }).then(function(queryURL) {
-        console.log("here is the data", queryURL)
+        for (var i = 0; i < queryURL.list.length; i++){
+        console.log(queryURL.list[i])
+      }
+
+      //var windEl = $('<div>').text(parseFloat(queryURL.city.list.deg));console.log("hope this worked" + windEl);
+      //var humidityEl = $('<div>').text(response.main.humidity);
+      //var tempEl = $('<div>').text(parseFloat(repsonse.main.temp - 273.15) * 1.8 + 32).toFixed(1);
+
+        // Create CODE HERE to calculate the temperature (converted from Kelvin)
+        //$('.temp').append(tempEl);
+        // Hint: To convert from Kelvin to Fahrenheit: F = (K - 273.15) * 1.80 + 32
+        // Create CODE HERE to dump the temperature content into HTML
+       // $('body').text(JSON.stringify(response));
         
       })
     };
+
+  
